e5828aaad1d5b2fb4a6374d36e0dc1d849a0208c
Michiel Uitdehaag <michiel@muisit.nl>
2025-10-14 15:41:18 +0200

    DID conversion to path: do not add .well-known for subpaths

--- src/convertors/convertDIDWeb.ts
+++ src/convertors/convertDIDWeb.ts
@@ -7,8 +7,14 @@ export async function convertFromDIDWeb(didUrl: string) {
 
   const keycode = didUrl.substring(8).split("#")[0];
   // replace any : with a /
-  const path = keycode.replaceAll(":", "/");
-  const url = "https://" + path + "/.well-known/did.json";
+  const path = keycode.replaceAll(":", "/").replaceAll('%3A', ':');
+  let url = "https://" + path + "/.well-known/did.json";
+  if (path.indexOf('/') >= 0) {
+    // if the url contains a subpath, do not append .well-known
+    // url cannot end with a slash (colon), so any slash means there is a subpath
+    // (but a slash at index 0 means there is no domain...)
+    url = "https://" + path + "/did.json";
+  }
 
   // fetch the document
   const response = await fetch(url).then((r) => r.json());
